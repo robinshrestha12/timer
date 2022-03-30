9d28a88d69c8737735db3d2d1ed61bfd688fb2c3
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-03-30 14:40:11 -0400

    fixed after review

--- timer1.js
+++ timer1.js
@@ -1,18 +1,18 @@
 const args = process.argv.slice(2); //making arrays of passed numbers.
 let sum = 0;
 if (args.length === 0) {
-  return "";
+  sum=0;
 }
-//only adding numerical value
-let sortedArgs = args.filter(function (item) { return (typeof item === "number") });
-//make array ascending..
-sortedArgs = args.map((ar) => Number(ar));
+
+//casting to number
+let sortedArgs = args.map((ar) => Number(ar));
+//sortedArgs =args.filter(a => !isNaN(a));
+sortedArgs = sortedArgs.filter(a => !isNaN(a));
 sortedArgs = sortedArgs.filter(function (number) { return number > 0; });//taking only positive numbers.
 sortedArgs = sortedArgs.sort(function (a, b) { return a - b });//sorted in ascending
 console.log(sortedArgs);
 for (let i = 0; i < sortedArgs.length; i++) {
-  //console.log(args[i]);
-  sum = sortedArgs[i] * 1000;
+  maxTimeValue= sortedArgs[i] * 1000;
   setTimeout(() => { // printing "." instead of sound, sound not supported in computer
     process.stdout.write(".");
   }, sortedArgs[i] * 1000);
@@ -20,4 +20,4 @@ for (let i = 0; i < sortedArgs.length; i++) {
 //// fixing the new line at the end.
 setTimeout(() => {
   process.stdout.write('\n');
-}, sum);
+}, maxTimeValue);
